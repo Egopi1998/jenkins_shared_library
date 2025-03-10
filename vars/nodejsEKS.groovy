49a90b3049ef220453a437281c86033c6162c937
E.Gopi <egopimani1998@gmail.com>
2025-03-11 00:26:27 +0530

    updated

--- vars/nodejsEKS.groovy
+++ vars/nodejsEKS.groovy
@@ -21,7 +21,7 @@ def call(Map configMap){
             stage('read the version'){
                 steps{
                     script{
-                        echo sh(returnStdout: true, script: 'env')
+                        // echo sh(returnStdout: true, script: 'env')
                         def packageJson = readJSON file: 'package.json'
                         appVersion = packageJson.version
                         echo "application version: $appVersion"
